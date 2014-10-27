af1cc4821af90ce1e6fd225257498ac9e271f53e
f.monteiro <fernando@newaeonweb.com.br>
2014-10-27 11:48:29 -0200

    add backbone-kickoff

---
+++ backbone-kickoff/js/kickoff-modules.js
@@ -0,0 +1,16 @@
+define([
+  'jQuery',
+  'Underscore',
+  'Backbone'
+  // Add as many dependencies as need
+], function($, _, Backbone){
+	
+	// Declare your Model, View, Collection, Router here
+	var MyModel = Backbone.Model.extend({
+
+	});
+
+
+	// return the module(Model/View/Collection/Router)name{};
+  return MyModel;
+});
